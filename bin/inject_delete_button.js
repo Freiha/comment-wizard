9d43ccfe151fddfa038ab16f2facf58f7c21511b
Freiha <116867487+Freiha@users.noreply.github.com>
2024-08-27 18:03:34 +0200

    Delete bin directory - renamed to src

--- bin/inject_delete_button.js
+++
@@ -1,6 +0,0 @@
-const deleteButtons = document.getElementsByClassName(
-  "ico_delete _btnDeleteSeries"
-);
-for (const deleteButton of deleteButtons) {
-  deleteButton.style.visibility = "hidden";
-}
